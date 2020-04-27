c72f3294a4437d4fea851664f64b955c5fa425a6
Elias Guere <eliasguere1025@gmail.com>
2020-04-26 20:09:11 -0500

    Conexion de nuestros servicios con mongoDB

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
+// Body parser
+app.use(express.json());
+
 moviesApi(app);
 
 app.listen(config.port, function() {
