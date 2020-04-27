e96a172b923dffeecfc7fd9e6989e3023a26667c
Elias Guere <eliasguere1025@gmail.com>
2020-04-27 01:11:25 -0500

    Implementando boom para el manejo de errores

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers');
+const notFoundHanlder = require('./utils/middleware/notFoundHanlder');
 
 // Body parser
 app.use(express.json());
 
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHanlder);
+
+// Errors middlewares
 app.use(logErrors);
+app.use(wrapErrors)
 app.use(errorHandler);
 
 app.listen(config.port, function() {
